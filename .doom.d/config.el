6a4459edd9965b18fbdfd96393d94979429dffd3
Andrew <andrew.s.lai5@gmail.com>
2021-01-12 11:15:48 -0600

    Don't use custom to set variables, use setq instead

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -362,13 +362,8 @@
     (print (frame-first-window new-frame))
     (switch-to-buffer repl)))
 
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(cider-repl-pop-to-buffer-on-connect nil)
- '(cider-connected-hook '(cider-repl-in-new-frame)))
+(setq cider-repl-pop-to-buffer-on-connect nil)
+(setq cider-connected-hook '(cider-repl-in-new-frame))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; EDiff
