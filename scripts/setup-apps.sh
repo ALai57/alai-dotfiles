47cea096ccf048257d6e9023589b3b22017a2091
Andrew Lai <alai@splashfinancial.com>
2023-05-12 17:19:30 -0500

    Emacs install

--- scripts/setup-apps.sh
+++ scripts/setup-apps.sh
@@ -18,6 +18,11 @@ function install_doom(){
 	doom sync
 }
 
+function install_emacs(){
+	# https://www.emacswiki.org/emacs/EmacsForMacOS
+	osascript -e "tell application \"Finder\" to make alias file to (POSIX file \"/opt/homebrew/Cellar/emacs-plus@30/30.0.50/Emacs.app\") at POSIX file \"$HOME/Applications\""
+}
+
 #install_emacs
 install_lein
 install_doom
